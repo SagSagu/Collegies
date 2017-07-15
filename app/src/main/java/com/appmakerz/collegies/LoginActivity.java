449916ab22be4a4c292e320aa186bebed7fd3829
Sagar A <sagsagusagar@gmail.com>
2017-07-15 18:41:04 +0530

    Initial commit

---
+++ app/src/main/java/com/appmakerz/collegies/LoginActivity.java
@@ -0,0 +1,179 @@
+package com.appmakerz.collegies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.support.percent.PercentLayoutHelper;
+import android.support.percent.PercentRelativeLayout;
+import android.widget.Toast;
+
+/**
+ * Created by daksha on 7/15/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean isSigninScreen = true;
+    private TextView tvSignupInvoker;
+    private LinearLayout llSignup;
+    private TextView tvSigninInvoker;
+    private LinearLayout llSignin;
+    private LinearLayout llSignupDetails;
+    private Button btnSignup;
+    private Button btnSignin;
+    private EditText etSignupEmail, etSignupPassword, etSignupcPassword, etSigninEmail, etSigninPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.login_layout);
+
+
+        etSignupEmail = (EditText) findViewById(R.id.singup_email);
+        etSignupPassword = (EditText) findViewById(R.id.singup_password);
+        etSignupcPassword = (EditText) findViewById(R.id.singup_cPassword);
+        etSigninEmail = (EditText) findViewById(R.id.signin_email);
+        etSigninPassword = (EditText) findViewById(R.id.signin_password);
+
+
+        tvSignupInvoker = (TextView) findViewById(R.id.tvSignupInvoker);
+        tvSigninInvoker = (TextView) findViewById(R.id.tvSigninInvoker);
+
+        btnSignup = (Button) findViewById(R.id.btnSignup);
+        btnSignin = (Button) findViewById(R.id.btnSignin);
+
+       /* btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Sign Up", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        llSignup = (LinearLayout) findViewById(R.id.llSignup);
+        llSignin = (LinearLayout) findViewById(R.id.llSignin);
+        llSignupDetails = (LinearLayout) findViewById(R.id.llSignupDetails);
+
+        btnSignin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);
+
+
+        tvSignupInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSigninScreen = false;
+                showSignupForm();
+            }
+        });
+
+        tvSigninInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSigninScreen = true;
+                showSigninForm();
+            }
+        });
+        showSigninForm();
+
+      /*  btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation clockwise= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_right_to_left);
+                if(isSigninScreen)
+                    btnSignup.startAnimation(clockwise);
+
+                Toast.makeText(getBaseContext(),"hdfjfhj",Toast.LENGTH_SHORT).show();
+
+            }
+        });*/
+    }
+
+    private void showSignupForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.15f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.85f;
+        llSignup.requestLayout();
+
+        tvSignupInvoker.setVisibility(View.GONE);
+        tvSigninInvoker.setVisibility(View.VISIBLE);
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_right_to_left);
+        llSignup.startAnimation(translate);
+
+        Animation clockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_right_to_left);
+        btnSignup.startAnimation(clockwise);
+
+    }
+
+    private void showSigninForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.85f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.15f;
+        llSignup.requestLayout();
+
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_left_to_right);
+        llSignin.startAnimation(translate);
+
+        tvSignupInvoker.setVisibility(View.VISIBLE);
+        tvSigninInvoker.setVisibility(View.GONE);
+        Animation clockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_left_to_right);
+        btnSignin.startAnimation(clockwise);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String email, password, cPassword;
+
+        switch (view.getId()) {
+
+            case R.id.btnSignin:
+               email = etSigninEmail.getText().toString();
+               password = etSignupPassword.getText().toString();
+
+                break;
+
+            case R.id.btnSignup:
+
+                startActivity(new Intent(this, SignUpDetailsActivity.class));
+                overridePendingTransition(R.anim.translate_right_to_left, R.anim.translate_left_to_right);
+
+                email = etSignupEmail.getText().toString();
+                password = etSignupPassword.getText().toString();
+                cPassword = etSignupcPassword.getText().toString();
+
+                if (password.equals(cPassword)) {
+                    Toast.makeText(getBaseContext(), "Sign Up", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "Sign Up Failed", Toast.LENGTH_SHORT).show();
+                }
+
+
+                break;
+
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/appmakerz/collegies/SignUpDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.appmakerz.collegies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_details_layout);
+    }
+}
