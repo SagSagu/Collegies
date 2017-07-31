53d863aacf580d2cf67d6996336217b74db95233
Sagar A <sagsagusagar@gmail.com>
2017-07-31 12:48:56 +0530

    Added Loading button

---
+++ app/src/main/java/com/appmakerz/collegies/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.appmakerz.collegies;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by daksha on 7/30/2017.
+ */
+public class SecondActivity  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
